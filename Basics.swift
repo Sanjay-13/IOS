8ea037a86a7e85d788f05f40cc8660afccb62000
Sanjay-13 <69335575+Sanjay-13@users.noreply.github.com>
2021-08-24 09:06:05 -0500

    Update Basics.swift

--- Basics.swift
+++ Basics.swift
@@ -26,3 +26,32 @@ let PI = 3.14
 var area = PI*r*r
 print(area)
 print("The area of circle is \(area)")
+var httpError  = (errorCode : 404  , errorMessage : "Page Not Found")
+print(httpError)
+print(httpError.errorCode , terminator : ": ")
+print(httpError.errorMessage )
+var name = ("John","Smith")
+var fName = name.0
+var lName = name.1
+print(fName , terminator : ",")
+print(lName)
+var origin = (x : 0 , y : 0)
+var point = origin
+print(point)
+let groceries = (1,2)
+print(groceries.0)
+print(groceries.1)
+print(type(of: groceries))
+let city = (name : "Maryville" , population : 11,000)
+let ( cityName ,cityPopulation ) = (city.0 , city.1)
+print(cityName)
+print(cityPopulation)
+var fname = "Joe"
+var lname = "Root"
+(fname , lname) = (lname , fname)
+print("First Name is \(fname) and Last Name is \(lname)")
+var cricketKit = (("handGloves" ,"helmet"),("bat","ball"))
+print(cricketKit.0.0)
+print(cricketKit.0.1)
+print(cricketKit.1.0)
+print(cricketKit.1.1)
