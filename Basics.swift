3465d3c9b0c9b9ae19e1996493df64e678e5e38c
Sanjay-13 <69335575+Sanjay-13@users.noreply.github.com>
2021-08-24 08:46:11 -0500

    Create Basics.swift

---
+++ Basics.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+var sum=0;
+var a=10;
+var b=20;
+sum = a+b;
+print(sum);
+print("The Answer is :\(sum)");
+print("The message is", terminator:" $");
+print(sum)
+print(1,2,3,4,8,9, separator:"::");
+print(1.0,2,7.2)
+var str1 = "Sanjay"
+var str2 = "Saripalli"
+print(str1,str2)
+print(str2,",", str1)
+print(str2,str1, separator: ", ")
+print(str2, terminator:", ")
+print(str1)
+var p=76
+var q=22
+p = p-q
+print(p)
+let r = 20.0
+let PI = 3.14
+var area = PI*r*r
+print(area)
+print("The area of circle is \(area)")
